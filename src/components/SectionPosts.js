47551aa2fcdef7ffcd31f92ed1746a497d86fba5
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 22:24:40 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -31,6 +31,13 @@ export default function SectionPosts(props) {
                     ))}
                 </div>
             </div>
+            <style global jsx>{`
+                :root {
+                    --posts-background-color: ${backgroundColor};
+                    --posts-title-color: ${titleColor};
+                    --posts-text-color: ${textColor};
+                };
+            `}</style>
         </section>
     );
 }
