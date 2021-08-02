c0d2f08acafa1cadd73abe35d358933bb6d798bd
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 20:38:44 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -13,7 +13,7 @@ export default function SectionHero(props) {
     const title = _.get(section, 'title');
     const content = _.get(section, 'content');
     const actions = _.get(section, 'actions');
-    const heroHeadingColor = _.get(config, 'heroHeadingColor');
+    const titleColor = _.get(section, 'titleColor');
 
     return (
         <section id={sectionId} className={`block hero-block bg-accent outer`}>
