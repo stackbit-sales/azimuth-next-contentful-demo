e082309f28c6fd3460fbbdfce9120eb884afe163
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 21:42:12 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -19,7 +19,7 @@ export default function SectionFeatures(props) {
     return (
         <section id={sectionId} className={`block features-block features-background-color outer`}>
             <div className="block-header inner-small">
-                {title && <h2 className="block-title features-text-color">{title}</h2>}
+                {title && <h2 className="block-title features-title-color">{title}</h2>}
                 {subtitle && <p className="features-text-color">{subtitle}</p>}
             </div>
             {features && (
