3226d1de5c4f27804a2f838cc303e1bee8afa822
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 21:36:27 +0000

    SectionReviews.js: changed

--- src/components/SectionReviews.js
+++ src/components/SectionReviews.js
@@ -10,7 +10,6 @@ export default function SectionReviews(props) {
     const title = _.get(section, 'title');
     const subtitle = _.get(section, 'subtitle');
     const reviews = _.get(section, 'reviews');
-    const backgroundColor = _.get(section, 'backgroundColor');
 
     return (
         <section id={sectionId} className={`block reviews-block bg-${background} outer`}>
