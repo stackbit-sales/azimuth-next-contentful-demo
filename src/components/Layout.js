0f1aa8457142c942225eaf2a8466cd0c801be849
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 17:25:40 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -80,7 +80,7 @@ export default class Body extends React.Component {
             return <meta key={metaIdx} {...nameAttr} content={value} />;
         });
         const colorPrimary = _.get(config, 'colorPrimary');
-        // const colorSecondary = _.get(config, 'colorSecondary');
+        const colorSecondary = _.get(config, 'colorSecondary');
 
         return (
             <React.Fragment>
