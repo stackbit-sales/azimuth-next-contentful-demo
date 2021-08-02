b3f8956c9dcdc84422d62583b8aa970d86a69498
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 22:02:30 +0000

    SectionFeatures.js: changed

--- src/components/SectionFeatures.js
+++ src/components/SectionFeatures.js
@@ -60,7 +60,7 @@ function FeatureItem(featureItem) {
                     </div>
                 )}
                 <div className="cell block-content">
-                    <h3 className="block-title underline feature-item-title-color">{title}</h3>
+                    <h3 className="block-title underline features-title-color">{title}</h3>
                     <div className="block-copy feature-item-text-color">
                         <ReactMarkdown>{content}</ReactMarkdown>
                     </div>
