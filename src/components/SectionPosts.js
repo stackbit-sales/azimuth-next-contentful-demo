00078e205b1d30c22b7fa084566516256ec8a150
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 13:01:12 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -8,6 +8,8 @@ import getPostUrl from '../utils/getPostUrl';
 
 export default function SectionPosts(props) {
     const section = _.get(props, 'section');
+    console.log('section', section);
+    
     const posts = _.orderBy(_.get(props, 'posts', []), 'date', 'desc');
     const recentPosts = posts.slice(0, 3);
     const sectionId = _.get(section, 'section_id');
