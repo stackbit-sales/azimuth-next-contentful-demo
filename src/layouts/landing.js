69a1a0234ea74a38aa76261488d9ab1486372422
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 13:06:36 +0000

    landing.js: changed

--- src/layouts/landing.js
+++ src/layouts/landing.js
@@ -22,7 +22,8 @@ export default function Landing(props) {
     const posts = _.get(props, 'posts');
     const sections = _.get(page, 'sections');
     const pageUrlPath = _.get(page, 'urlPath');
-
+    console.log('sections', sections);
+    
     return (
         <Layout page={page} config={config}>
             {_.map(sections, (section, index) => {
