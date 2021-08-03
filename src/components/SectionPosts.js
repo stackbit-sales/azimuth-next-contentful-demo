794c55bcb3ea3c82b94e16e0e46c4eef8e9c8858
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 13:28:31 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -51,7 +51,6 @@ function RecentPost(post) {
     const titleColor = _.get(post, 'titleColor');
     const textColor = _.get(post, 'textColor');
     const subTextColor = _.get(post, 'subTextColor');
-    console.log('post', post);
     
     return (
         <article className="cell post">
