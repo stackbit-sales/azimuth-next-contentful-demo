1c734c98c4df669d4bad1ebef81d3243708f51eb
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 12:47:15 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -14,6 +14,8 @@ export default function Post(props) {
     const image = _.get(page, 'image');
     const imageAlt = _.get(page, 'image_alt');
     const content = _.get(page, 'content');
+    console.log(page);
+    
 
     return (
         <Layout page={page} config={config}>
