e6c3dbc6810d225ebdb42a8b2c8e1b7f2f8ba825
Stackbit Code Editor <projects@stackbit.com>
2021-08-03 04:01:36 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -35,7 +35,7 @@ export default function Action(props) {
                 button: actionStyle === 'primary' || actionStyle === 'secondary',
                 secondary: actionStyle === 'secondary',
             })}
-             style={{borderRadius: borderRadius}, {backgroundColor: backgroundColor}, {textColor: textColor}, {backgroundColor: backgroundColor}}
+             style={{borderRadius: borderRadius}, {backgroundColor: backgroundColor}, {textColor: textColor}, {borderColor: borderColor}}
         >
             <span className={classNames({ 'order-first': actionIconPos === 'right' })}>{label}</span>
         </Link>
