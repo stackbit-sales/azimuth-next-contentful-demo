e764d23cba0e2853463976bdd8b9cde20cb38c80
Stackbit Code Editor <projects@stackbit.com>
2021-08-05 23:57:04 +0000

    SectionFaq.js: changed

--- src/components/SectionFaq.js
+++ src/components/SectionFaq.js
@@ -56,7 +56,6 @@ function FaqItem(faqItem) {
     const titleColor = _.get(faqItem, 'titleColor');
     const iconColor = _.get(faqItem, 'iconColor');
     const iconBackgroundColor = _.get(faqItem, 'iconBackgroundColor');
-    const borderColor = _.get(faqItem, 'borderColor');
     let borderRadius = _.get(faqItem, 'borderRadius');
     console.log('borderColor', borderColor);
     
