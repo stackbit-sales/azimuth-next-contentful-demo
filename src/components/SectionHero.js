7dd0fcbe3956956488113e9e3b7cf345d64e858b
Stackbit Code Editor <projects@stackbit.com>
2021-08-06 12:13:56 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -60,7 +60,7 @@ export default function SectionHero(props) {
                             </div>
                         )}
                         {actions && (
-                            <div className="block-buttons" style={{backgroundColor: `gold`, justifyContent: `flex-start`}}>
+                            <div className="block-buttons" style={{backgroundColor: `gold`, justifyContent: `flex-end`}}>
                                 <CtaButtons actions={actions} />
                             </div>
                         )}
