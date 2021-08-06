db306e9a8685a86897baaaa4874d67a53444de95
Stackbit Code Editor <projects@stackbit.com>
2021-08-06 00:18:23 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -35,7 +35,7 @@ export default function SectionHero(props) {
     
 
     return (
-        <section id={sectionId} className={`block hero-block outer`} style={{background: `linear-gradient( to right, ${backgroundColorLeft}, ${backgroundColorRight})`, flexDirection: stackedOrFlat}}>
+        <section id={sectionId} className={`block hero-block outer`} style={{background: `linear-gradient( to right, ${backgroundColorLeft}, ${backgroundColorRight})`, flexDirection: column !important}}>
             <div className="inner">
                 <div className="grid order-container">
                     {image && (
