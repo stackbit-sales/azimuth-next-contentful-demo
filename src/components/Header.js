20c1f295c3bf3630346c53a5182c63a7cec174fb
Stackbit Code Editor <projects@stackbit.com>
2021-08-06 13:03:07 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,7 +46,7 @@ export default class Header extends React.Component {
         document.body.classList.remove('menu--opened');
     }
 
-    renderNavLinks(navLinks, currentPageUrl, navBarTextColor, navBarTextColorActive) {
+    renderNavLinks(navLinks, currentPageUrl, navBarTextColor) {
         return (
             <React.Fragment>
                 <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation" style={{color: navBarTextColor}}>
