1453b1f6974be0044ebac0038b268bf27aab0fff
Stackbit Code Editor <projects@stackbit.com>
2021-08-06 13:03:15 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -95,7 +95,6 @@ export default class Header extends React.Component {
         const pageTemplate = _.get(page, 'template');
         let navBarBackgroundColor = _.get(header, 'navBarBackgroundColor');
         let navBarTextColor = _.get(header, 'navBarTextColor');
-        let navBarTextColorActive = _.get(header, 'navBarTextColorActive');
 
         return (
             <header id="masthead" className="site-header outer" style={{backgroundColor: navBarBackgroundColor}}>
