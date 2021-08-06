966114384436778323da7f3d464cc2b8a735af56
Stackbit Code Editor <projects@stackbit.com>
2021-08-06 13:01:26 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -119,7 +119,7 @@ export default class Header extends React.Component {
                                 </p>
                             )}
                         </div>
-                        {!hideNavLinks && !_.isEmpty(navLinks) && this.renderNavLinks(navLinks, currentPageUrl)}
+                        {!hideNavLinks && !_.isEmpty(navLinks) && this.renderNavLinks(navLinks, currentPageUrl, navBarTextColor)}
                     </div>
                 </div>
             </header>
